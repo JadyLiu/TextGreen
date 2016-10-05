137156a154b445779bc4a1b933eb3976d3056c7f
Liu, Jady <jady.liu@news.com.au>
2016-10-05 20:18:35 +1100

    Send SMS via SNS

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TextGreen
+This is an Alexa skills backed by Lambda function
+
+## Feature
+This skill sends any SMS message via AWS SNS to any mobile number you define
--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-var LIST_OF_RESPONSES = [
-    "I miss you",
-    "How are you today",
-    "What are you doing"
-];
 
 var AWS = require("aws-sdk");
 
@@ -10,8 +5,6 @@ var AWS = require("aws-sdk");
  * This sample shows how to create a simple Lambda function for handling speechlet requests.
  */
 
-// Route the incoming request based on type (LaunchRequest, IntentRequest,
-// etc.) The JSON body of the request is provided in the event parameter.
 exports.handler = function (event, context) {
     try {
         console.log("event.session.application.applicationId=" + event.session.application.applicationId);
@@ -70,11 +63,8 @@ function onIntent(intentRequest, session, callback) {
         intentName = intentRequest.intent.name;
 
     if ("TextMeIntent" === intentName) {
-         console.log("TextMeIntent");
-         setMessageInSession(intent, session, callback);
-//=========================================================
-         // var MessageToMe = intent.slots.Message;
-         // TextGreen(MessageToMe, callback);
+        console.log("TextMeIntent received");
+        setMessageInSession(intent, session, callback);
     } else if ("AMAZON.HelpIntent" === intentName) {
         getWelcomeResponse(callback);
     } else {
@@ -92,23 +82,17 @@ function onSessionEnded(sessionEndedRequest, session) {
     // Add cleanup logic here
 }
 
-// --------------- Functions that control the skill's behavior -----------------------
-
 function getWelcomeResponse(callback) {
     // If we wanted to initialize the session to have some attributes we could add those here.
     var sessionAttributes = {};
     var speechOutput = "Hello JD " +
         "I'm excited to help you sending message. What would you want to send?";
-    // If the user either does not reply to the welcome message or says something that is not
-    // understood, they will be prompted again with this text.
     var repromptText = "Tell me if you have any message to send ";
     var shouldEndSession = false;
 
     callback(sessionAttributes,
         buildSpeechletResponse(speechOutput, repromptText, shouldEndSession));
 }
-
-// --------------- Helpers that build all of the responses -----------------------
 
 function buildSpeechletResponse(output, repromptText, shouldEndSession) {
     return {
@@ -138,53 +122,51 @@ function buildResponse(sessionAttributes, speechletResponse) {
  * Sets the message in the session and prepares the speech to reply to the user.
  */
 function setMessageInSession(intent, session, callback) {
-    console.log(intent)
+    console.log(intent);
 
-    // var cardTitle = intent.name;
-    var messageSlot = intent.slots.Message;
+    var text = intent.slots.Text.value;
+    var destination = intent.slots.Destination.value;
 
-    console.log(messageSlot.value)
-    // console.log("I think"+messageSlot)
+    console.log("Sending sms to : "+ destination +" with text: "+ text);
+
     // var repromptText = "";
     // var sessionAttributes = {};
     var shouldEndSession = false;
     // var speechOutput = "";
-    if (messageSlot.value) {
+    var number;
+    // messageSlot.value
+    var sns = new AWS.SNS();
 
-        var sns = new AWS.SNS();//call AWS sdk
+    if ("JD" === destination) {
+        number = "+61433012483";
         var params = {
-            Message: 'JD sends you a message ' + messageSlot.value, 
-            TopicArn: 'arn:aws:sns:us-east-1:097509526832:TextMe'
-        };
-        
-        console.log("sendMessage funtion")
+           Message: 'JD sends you a message ' + text,
+           PhoneNumber: number
+         // TopicArn: 'arn:aws:sns:us-east-1:097509526832:TextMe'
+        };  
+        console.log(text);
 
-        message = messageSlot.value;
-
-        Message = messageSlot.value;
-
-        console.log(Message)
-
-        console.log("Message slot contains: " + messageSlot.value + ".");
         sns.publish(params, function (err, data) {
-            if (err) console.log(err, err.stack); // an error occurred
+            if (err) console.log(err, err.stack);
             else
             {   
                 console.log("Success"); 
                 
-                 if ("How are you" === Message)
-                    {
-                        speechOutput = "I have a good day, wish you feel great as well!" +
-                        "I will ask the question to you.";
+                if ("What are we going to eat" === text){
+                        speechOutput = "I feel hungry too, are you going to cook lots of yummy food for JD" +
+                        "Let JD know";
                     } 
-                    else if ("What are you doing" === Message)
-                    { 
+                else if ("I miss you" === text) { 
                         speechOutput = "I assistant JD to Message " +
-                        "I will ask the question to you.";
+                        "I think JD is really missing you";
+                    }
+                else if ("I am at home" === text) { 
+                        speechOutput = "I assistant JD to Message " +
+                        "JD is at home, hurry up to go home";
                     }
                     
                     var sessionAttributes = {};
-                    var speechOutput = speechOutput + "Ok, the message was sent.";
+                    var speechOutput = speechOutput + "and Ok, now, the message was sent.";
                             
                     var repromptText = "";
                     var shouldEndSession = true;
@@ -211,7 +193,47 @@ function setMessageInSession(intent, session, callback) {
         // });
         // req.write('{"channel": "#aws-lambda", "username": "webhookbot", "text": "[via Alexa]: ' + message + '", "icon_emoji": ":ghost:"}');
         // req.end();
-    } else {
+    } else if ("love" === destination) {
+        number = "+61452377055";
+        var params = {
+           Message: 'Alexa helps JD send you a message: ' + text,
+           PhoneNumber: number
+         // TopicArn: 'arn:aws:sns:us-east-1:097509526832:TextMe'
+        };  
+        console.log(text);
+
+        sns.publish(params, function (err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else
+            {   
+                console.log("Success"); 
+                
+                if ("What are we going to eat" === text){
+                        speechOutput = "I feel hungry too, are you going to cook lots of yummy food for JD" +
+                        "Let JD know";
+                    } 
+                else if ("I miss you" === text) { 
+                        speechOutput = "I assistant JD to Message " +
+                        "I think JD is really missing you";
+                    }
+                else if ("I am at home" === text) { 
+                        speechOutput = "I assistant JD to Message " +
+                        "JD is at home, hurry up to go home";
+                    }
+                    
+                    var sessionAttributes = {};
+                    var speechOutput = speechOutput + "Ok, the message was sent.";
+                            
+                    var repromptText = "";
+                    var shouldEndSession = true;
+                    
+                    callback(sessionAttributes,
+                        buildSpeechletResponse(speechOutput, repromptText, shouldEndSession));
+ 
+            }
+        });
+    } 
+    else {
             var sessionAttributes = {};
             var shouldEndSession = false;
             speechOutput = "I'm not sure what message you are going to send. Please try again";
@@ -219,11 +241,6 @@ function setMessageInSession(intent, session, callback) {
             "you had any message to send.";
             callback(sessionAttributes,
                         buildSpeechletResponse(speechOutput, repromptText, shouldEndSession));
-    //     speechOutput = "I didn't hear your message clearly, please try again";
-    //     repromptText = "I didn't hear your message clearly, you can give me your "
-    //             + "message by saying, my message is...";
-    // callback(sessionAttributes, 
-    //          buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
     }
 }
 
